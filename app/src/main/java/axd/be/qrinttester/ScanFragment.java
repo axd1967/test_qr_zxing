8bc18adace73918ad5f292cb40b9ef20497cef0c
Alexander Duytschaever <alexander.duytschaever@ugent.be>
2016-10-27 13:46:19 +0200

    does not work as explained in https://github.com/zxing/zxing/wiki/Scanning-Via-Intent#intentintegrator

--- app/src/main/java/axd/be/qrinttester/ScanFragment.java
+++ app/src/main/java/axd/be/qrinttester/ScanFragment.java
@@ -1,5 +1,6 @@
 package axd.be.qrinttester;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v4.app.Fragment;
@@ -8,6 +9,9 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by alex on 27/10/2016.
@@ -39,9 +43,35 @@ public class ScanFragment extends Fragment {
 
     }
 
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+
+        Log.d(TAG, "onActivityResult: request: " + requestCode);
+
+        switch (requestCode) {
+
+            case IntentIntegrator.REQUEST_CODE: // XXX see also https://github.com/zxing/zxing/issues/291#issuecomment-256361115
+//            case QR_SCANNER_REQUEST_CODE:
+
+                IntentResult scanResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+                if (scanResult != null) {
+                    // handle scan result
+                    Log.d(TAG, "scan: "+scanResult.getContents());
+                    return;
+                }
+                Log.d(TAG, "scan: no scan");
+                break;
+        }
+
+        super.onActivityResult(requestCode, resultCode, data);
+    }
+
     public ScanFragment() {}
 
     private void scan() {
         Log.d(TAG, "scan: start");
+
+        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+        integrator.initiateScan();
     }
 }
